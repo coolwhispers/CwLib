5be0473e6affdeb7d31509e187144cc68fd40f70
U+ <cool-whispers@hotmail.com>
2018-06-11 20:46:55 +0800

    update

---
+++ CwLib.Cache.Redis/CacheAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CwLib.AOP;
+
+namespace CwLib.Cache.Redis
+{
+    public class CwCacheMethodAttribute : CwAopAttribute
+    {        
+        public override void OnBegin()
+        {
+        }
+
+        public override void OnExecuting()
+        {
+        }
+
+        public override void OnExecuted()
+        {
+        }
+
+        public override void OnResult(ref object result)
+        {
+        }
+
+        public override void OnExecption(Exception e)
+        {
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
